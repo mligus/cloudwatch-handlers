d9e36f5be2d39a85ffcb0021e41d13ea584a1e44
Max Ligus <max.ligus@gmail.com>
2017-05-28 12:12:52 +0300

    add packaging functionality

--- Makefile
+++ Makefile
@@ -1,7 +1,43 @@
+clean-pyc:
+	find . -name '*.pyc' -exec rm -f {} +
+	find . -name '*.pyo' -exec rm -f {} +
+	find . -name '*~' -exec rm -f {} +
+	find . \( -name \*.pyc -o -name \*.pyo -o -name __pycache__ \) -prune -exec rm -rf {} +
+
+clean-build:
+	rm -fr build/
+	rm -fr dist/
+	rm -fr *.egg-info
+	rm -fr *.egg
+
+clean-test:
+	rm -fr coverage_html/
+	rm -f .coverage*
+
+clean: clean-pyc clean-build clean-test
+
 init:
 	pip install -r requirements.txt
 
-test:
+lint:
+	python setup.py flake8
+
+test: clean
 	py.test tests
 
-.PHONY: init test
+wheel: clean
+	python setup.py bdist_wheel
+
+help:
+	@echo "    clean"
+	@echo "        Clean working directory from build and tests artifacts"
+	@echo "    init"
+	@echo "        Initialize development environment (pip, etc.)"
+	@echo "    lint"
+	@echo "        Run linter (flake8)"
+	@echo "    test"
+	@echo "        Run unit tests"
+	@echo "    wheel"
+	@echo "        Make wheel package"
+
+.PHONY: clean init lint test wheel
---
+++ setup.py
@@ -0,0 +1,67 @@
+"""CloudWatch logging handlers setup module.
+
+Based on setuptools
+"""
+
+from setuptools import setup, find_packages
+from codecs import open
+from os import path
+
+
+here = path.abspath(path.dirname(__file__))
+
+
+# Get the long description from the README file
+with open(path.join(here, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+
+setup(
+    name='cloudwath_handlers',
+
+    version='0.1.0',
+
+    description='Python logging handlers for AWS CloudWatch service',
+    long_description=long_description,
+
+    url='https://github.com/mligus/cloudwatch-handlers',
+
+    author='Max Ligus',
+    author_email='max.ligus@gmail.com',
+
+    license='MIT',
+
+    # Description  at https://pypi.python.org/pypi?%3Aaction=list_classifiers
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Libraries',
+        'Topic :: System :: Logging',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+    ],
+
+    keywords='logging handlers aws cloudwatch development',
+
+    packages=find_packages(exclude=['docs', 'tests']),
+
+    setup_requires=[
+        'pytest-runner',
+    ],
+
+    install_requires=[
+        'boto3>=1.4.4',
+    ],
+
+    tests_require=[
+        'pytest>=3.1.0',
+        'pytest-cov>=2.5.1',
+     ],
+
+)
